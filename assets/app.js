f7e70d107827388f5f905289ba5ac32a47d2523f
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-11 20:04:50 -0400

    fetch weather api

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,32 @@
 var searchInput;
-var city = $('#city');
 var apiKey = 'f1a95fc68a0873c27d61c20f9a6dd653';
-var url = 'api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&appid=' + apiKey;
+var lat;
+var lon;
 
 
+function getWeather(lat, lon) {
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (data){
+            console.log(data)
+        })
+}
 
 
-// Search button 
 $('#search-btn').on('click', function(event){
     event.preventDefault();
     searchInput = $('#city').val();
-    console.log(searchInput);
-    getWeather()
+    var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + '&appid=' + apiKey;
+    fetch(url)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        lat = data[0].lat;
+        lon = data[0].lon;
+        getWeather(lat,lon);
+    })
 });
+
